ae05388ae761481e3f77d3121e05fa209eb66034
Tim Bazuin <krageon@gmail.com>
2013-05-23 14:35:46 +0200

    things! :D

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject battleship "0.1.0-SNAPSHOT"
-            :description "FIXME: write this!"
+(defproject battleship "0.1.0"
+            :description "Battleship game written using clojure and noir"
             :dependencies [[org.clojure/clojure "1.4.0"]
                            [noir "1.3.0-beta3"]]
             :main battleship.server)
