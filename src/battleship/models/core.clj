a5e73354d68992726bd277203229f626378b6af6
Tim Bazuin <krageon@gmail.com>
2013-05-30 11:21:11 +0200

    core logic base

---
+++ src/battleship/models/core.clj
@@ -0,0 +1,23 @@
+;; Battleship in clojure core
+
+;; Printing the board
+
+(def bs-board-atom (atom {}))
+
+(def bs-board-print
+  (let [default "~"]
+    (fn [] (
+            loop
+            )
+      )))
+
+(bs-board-print)
+
+;; Picking ship locations
+
+
+;; Randomised ship setup
+
+
+;; Making a move
+
