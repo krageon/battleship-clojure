21d9274ec2ca0173bd2e7c0c7baee854c40057e8
Tim Bazuin <krageon@gmail.com>
2013-05-30 11:47:23 +0200

    cleanup of test functions, added some comments, changed newline into a constant defined at the top (in case we want to make this into <br /> or whatever)

--- src/battleship/models/core.clj
+++ src/battleship/models/core.clj
@@ -2,34 +2,38 @@
 
 ;; Printing the board
 
-(def bs-board-test {[0 0] "*"})
+; constants
+(def default "~")
+(def boat "o")
+(def hit "*")
+(def new-line "\n")
 
-(def bs-board-atom (atom bs-board-test))
+(def bs-board-a (atom {}))
+(def bs-board-b (atom {}))
 
 (def bs-cell-get
-  (let [default "~"]
-    (fn [coordinates]
-      (let [at-coordinates (@bs-board-atom coordinates)]
-        (if (nil? at-coordinates)
-          default
-          at-coordinates)))))
+  (fn [board coordinates]
+    (let [at-coordinates (@board coordinates)]
+      (if (nil? at-coordinates)
+        default
+        at-coordinates))))
 
+; converting the board (a sparse array-thing) to a string, filling in the gaps
 (def bs-board-string
-  (fn [] (loop [x 0
-                maxX 20
-                y 0
-                maxY 20
-                string ""]
-           (if (> y maxY)
-             string
-             (if (> x maxX)
-               (recur 0 maxX (inc y) maxY (str string "\n"))
-               (recur (inc x) maxX y maxY (str string (bs-cell-get [x y]))))
-             )
-           )
+  (fn [board]
+    (loop [x 0
+           maxX 20
+           y 0
+           maxY 20
+           string ""]
+      (if (> y maxY)
+        string
+        (if (> x maxX)
+          (recur 0 maxX (inc y) maxY (str string new-line))
+          (recur (inc x) maxX y maxY (str string (bs-cell-get board [x y]))))
+        )
+      )
     ))
-
-(println (bs-board-string))
 
 ;; Picking ship locations
 
