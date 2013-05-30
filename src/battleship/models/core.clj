6392f5840a013a6390dd0d053d36caf4976b3442
Tim Bazuin <krageon@gmail.com>
2013-05-30 11:39:20 +0200

    rudimentary single-board logic in (20x20)

--- src/battleship/models/core.clj
+++ src/battleship/models/core.clj
@@ -2,16 +2,34 @@
 
 ;; Printing the board
 
-(def bs-board-atom (atom {}))
+(def bs-board-test {[0 0] "*"})
 
-(def bs-board-print
+(def bs-board-atom (atom bs-board-test))
+
+(def bs-cell-get
   (let [default "~"]
-    (fn [] (
-            loop
-            )
-      )))
+    (fn [coordinates]
+      (let [at-coordinates (@bs-board-atom coordinates)]
+        (if (nil? at-coordinates)
+          default
+          at-coordinates)))))
 
-(bs-board-print)
+(def bs-board-string
+  (fn [] (loop [x 0
+                maxX 20
+                y 0
+                maxY 20
+                string ""]
+           (if (> y maxY)
+             string
+             (if (> x maxX)
+               (recur 0 maxX (inc y) maxY (str string "\n"))
+               (recur (inc x) maxX y maxY (str string (bs-cell-get [x y]))))
+             )
+           )
+    ))
+
+(println (bs-board-string))
 
 ;; Picking ship locations
 
