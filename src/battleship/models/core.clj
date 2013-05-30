44d9e0b66e6ce69770abacf093491e81acd5b1a6
Tim Bazuin <krageon@gmail.com>
2013-05-30 13:16:34 +0200

    ship placement and legality logic

--- src/battleship/models/core.clj
+++ src/battleship/models/core.clj
@@ -5,8 +5,8 @@
 ; constants
 (def default "~")
 (def boat "o")
-(def hit "*")
-(def shot "x")
+(def hit "x")
+(def shot "/")
 (def new-line "\n")
 
 (def bs-player-a (atom {:board {}, :shot {}}))
@@ -14,7 +14,7 @@
 
 (def bs-cell-get
   (fn [board coordinates]
-    (let [at-coordinates (@board coordinates)]
+    (let [at-coordinates (board coordinates)]
       (if (nil? at-coordinates)
         default
         at-coordinates))))
@@ -43,11 +43,51 @@
             {:name "destroyer", :size 2, :amount 2},
             {:name "submarine", :size 1, :amount 2}])
 
+(defn bs-ship-get
+  [n] (first (filter #(= n (:name %)) ships)))
+
+(defn bs-ship-coordinate-offset [orientation coordinates offset]
+  (if (= "horizontal" orientation)
+    [(+ (get coordinates 0) offset) (get coordinates 1)]
+    [(get coordinates 0) (+ (get coordinates 1) offset)]))
+
+(defn bs-ship-put-is-legal [player ship orientation coordinates]
+  (loop [offset 0]
+    (if-not (>= offset (:size (bs-ship-get ship)))
+      (if (= default (bs-cell-get (:board @player) (bs-ship-coordinate-offset orientation coordinates offset)))
+        (recur (inc offset))
+        (do (println "false") false))
+      true))
+  )
+
+(def bs-ship-put
+  (fn [player ship orientation coordinates] ; coordinates are top or left, respectively
+    (if (bs-ship-put-is-legal player ship orientation coordinates)
+      (do
+        (loop [offset 0]
+          (if-not (>= offset (:size (bs-ship-get ship)))
+            (do
+              (swap! player #(assoc-in % [:board (bs-ship-coordinate-offset orientation coordinates offset)]
+                                       boat))
+              (recur (inc offset)))
+            ))
+        true)
+      false)))
+
+(do
+  (bs-ship-put bs-player-a "battleship" "horizontal" [0 1])
+  (bs-ship-put bs-player-a "battleship" "vertical" [0 0]))
+
+(println (bs-board-string (:board @bs-player-a)))
 
 ;; Making a move
 (def bs-shoot
-  (fn [player]
-    ))
+  (fn [coordinates player opponent]
+    (swap! player
+           #(assoc-in % [:shot coordinates]
+                      (if (= boat (bs-cell-get (:board @opponent)))
+                        hit
+                        shot)))))
 
 ;; Randomised ship setup
 
