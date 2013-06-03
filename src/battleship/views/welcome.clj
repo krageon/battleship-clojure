9c4430f8a21146c8938e3b90ac53f49bf2a5ea9b
EvilTuinhekjeNL <RobUithol@gmail.com>
2013-06-03 20:54:23 +0200

    Board coordinates added

--- src/battleship/views/welcome.clj
+++ src/battleship/views/welcome.clj
@@ -1,15 +1,18 @@
 (ns battleship.views.welcome
   (:require [battleship.views.common :as common])
+  (:use [hiccup.core])
   (:use [noir.core :only [defpage]]))
 
 (defn createBoard[]
   "Returns a clean board"
-  (str
-     "<table>"
-     (reduce str(repeat 11(str "<tr>"(reduce str(repeat 11 "<td>&nbsp;</td>")) "</tr>")))
-     "</table>"
-   )
-  )
+  (html [:table
+         [:tr
+          [:th "&nbsp;&nbsp;"]
+          (for [x (range 1 11)] [:th x])
+         ]
+         (for [x (map char(concat (range 65 75)))] [:tr [:th x] (for [x (range 10)] [:td "&nbsp;&nbsp;"])])
+        ])
+ )
 
 (defpage "/" []
   (common/layout
