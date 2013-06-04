7974fa530325bf31df9a9694f7b01d6dbf29cc86
EvilTuinhekjeNL <robuithol@gmail.com>
2013-06-04 21:43:46 +0200

    Gave cells actions, made cell maker

--- src/battleship/views/pages.clj
+++ src/battleship/views/pages.clj
@@ -3,21 +3,34 @@
   (:use [hiccup.core])
   (:use [noir.core :only [defpage]]))
 
-;; functions
+(defn cell-html[yCoord xCoord value withSubmit?]
+  [:td {:name (str yCoord xCoord) :type (if withSubmit? "submit" "") :value value} (str "&nbsp;" value "&nbsp;")]
+  )
+
 (defn createBoard[]
-  "Returns a board, [] == Empty, [coordinateAction] == [Y X Action]"
+  "Returns a clean table board"
   []
   (html [:table
          [:tr
-          [:th "&nbsp;&nbsp;"]
+          [:th {:style "border: 0px"} "&nbsp;&nbsp;"]
           (for [x (range 1 11)] [:th x])
           ]
          (for [y (map char(range 65 75))] [:tr
                                            [:th y]
-                                           (for [x (range 1 11)] [:td {:id (str y x) } "&nbsp;&nbsp;"])
+                                           (for [x (range 1 11)] (cell-html y x "~" true))
                                            ])
          ])
   )
+
+(defn makeMove
+  "Give Coordinates and the action, and it will set it."
+  [params]
+  (let[x (second params), y (first params), action (last params)]
+    into [:td#A1 action])
+  )
+
+
+
 
 ;; index page
 (defpage "/" []
