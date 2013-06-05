f4ad4dde49addd3006e333b7b74e79da8e8ab710
EvilTuinhekjeNL <robuithol@gmail.com>
2013-06-05 18:29:13 +0200

    Made a TODO list for controller functions

--- src/battleship/models/controller.clj
+++ src/battleship/models/controller.clj
@@ -7,6 +7,16 @@
              ;[battleship.models.core :as model]
              ))
 
+;; Tim TODO:
+; Een functie maken die de schepen die ik meegeef in de state zet (allies board)
+; Vanuit de gui geef ik dit mee:
+; input: (defn ships [] [{:name "Aircraft Carrier" :xy "B6" :horizontal true}
+;                        {:name "Submarine" :xy "A1" :horizontal false}] etc... )
+;
+; Een functie die een schot registreert, checkt of iemand gewonnen heeft en anders
+; de computer een zet laat doen
+; input: (defn shoot [coordinates]) ;;=> "A6"
+
 (defn start-page[]
   ;;  (model/reset-game!)
   (view/start-screen))
