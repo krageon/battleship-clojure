a5436ac7468f746ea7cb1beb231bff1b8bc00d96
EvilTuinhekjeNL <robuithol@gmail.com>
2013-06-07 15:15:33 +0200

    Added a max amount of ships, and a todo improvement

--- resources/public/js/custom.js
+++ resources/public/js/custom.js
@@ -1,9 +1,22 @@
+/* TODO: instead of letting the user click, already place the boats and let them drag. */
 $(document).ready(function() {
-  $("input").click(function(){
+  var $maxSize = 18;
+  var $total = 0;
+  $("table#fleet td input").click(function(){
     if($(this).val() == "~"){
-      $(this).val("o");
+      if($total < $maxSize){
+        $(this).val("o");
+        $total++;
+      }else{
+        alert("You've already place the maximum of boats!!");
+      }
     }else{
       $(this).val("~");
+      $total--;
     }
+  });
+
+  $("#right-menu input[type='submit']").click(function(){
+    alert("Aantal bootjes: "+$("td input[value='o']").length+ " and total: "+$total + " and max: "+$maxSize);
   });
 });
